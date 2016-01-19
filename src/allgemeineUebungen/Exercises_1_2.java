b53415e4043af6182a182316bcd0a04bf674c646
Andreas-Keller <andreaskeller2010@gmail.com>
2016-01-19 12:27:02 +0100

    Second Commit from Nexus

--- src/allgemeineUebungen/Exercises_1_2.java
+++ src/allgemeineUebungen/Exercises_1_2.java
@@ -3,6 +3,6 @@ package allgemeineUebungen;
 public class Exercises_1_2 {
 	
 	public static void main( String args[]){
-		System.out.println("Hi World");
+		System.out.println("Hello World");
 	}
 }
