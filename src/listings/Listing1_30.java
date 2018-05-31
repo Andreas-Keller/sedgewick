a876a4f542f3ed572c062208809c1a80f00c0838
Andreas-Keller <andreaskeller2010@gmail.com>
2018-05-31 07:28:54 +0200

    add lib

--- src/listings/Listing1_30.java
+++ src/listings/Listing1_30.java
@@ -8,32 +8,33 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Listing1_30 {
 
-	public static void main (String args[]) {
+	public static void main (String args[]) throws Exception {
 		
 		Stack<String> ops = new Stack<String>();
 		Stack<Double> vals = new Stack<Double>();
 		
-		while (!StdIn.isEmpty()) {	
-			String s = StdIn.readString();
+		String s = StdIn.readString();
+		while ( !s.equals("evaluate")) {
 			if (s.equals("("));
 			else if (s.equals("+")) ops.push(s);
 			else if (s.equals("-")) ops.push(s);
 			else if (s.equals("*")) ops.push(s);
 			else if (s.equals("/")) ops.push(s);
 			else if (s.equals("sqrt")) ops.push(s);
-			else if (s.equals(")")){
+			else if (s.equals(")"))
+			{
 				String op = ops.pop();
 				double v = vals.pop();
-				if (s.equals("+")) v = vals.pop() + v;
-				else if (op.equals("-")) v = vals.pop() - v;
-				else if (op.equals("*")) v = vals.pop() * v;
-				else if (op.equals("/")) v = vals.pop() / v;
+				if      (op.equals("+"))    v = vals.pop() + v;
+				else if (op.equals("-"))    v = vals.pop() - v;
+				else if (op.equals("*"))    v = vals.pop() * v;
+				else if (op.equals("/"))    v = vals.pop() / v;
 				else if (op.equals("sqrt")) v = Math.sqrt(v);
 				vals.push(v);
 			}
 			else vals.push(Double.parseDouble(s));
+			s = StdIn.readString();
 		}
 		StdOut.println(vals.pop());
-	}
-	
+	}	
 }
